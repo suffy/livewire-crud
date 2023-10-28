12e084e8744e3d49b4170732b42beaad721e0f1a
suffy <suffy.yanuar@gmail.com>
2023-10-28 20:18:48 +0700

    add fitur update

--- app/Livewire/Employee.php
+++ app/Livewire/Employee.php
@@ -14,6 +14,8 @@ class Employee extends Component
     public $nama;
     public $email;
     public $alamat;
+    public $updateData = false;
+    public $employee_id;
 
     public function store()
     {
@@ -31,6 +33,49 @@ class Employee extends Component
         $validated = $this->validate($rules, $pesan);
         ModelsEmployee::create($validated);
         session()->flash('message','data berhasil dimasukkan');
+        
+        $this->clear();
+    }
+
+    public function edit($id)
+    {
+        $data = ModelsEmployee::find($id);
+        $this->nama = $data->nama;
+        $this->email = $data->email;
+        $this->alamat = $data->alamat;
+
+        $this->updateData = true;
+        $this->employee_id = $id;
+    }
+
+    public function update()
+    {
+        $rules = [
+            'nama'  => 'required',
+            'email'  => 'required|email',
+            'alamat'  => 'required',
+        ];    
+        $pesan = [
+            'nama.required'  => 'Nama wajib diisi',
+            'email.required'  => 'Email wajib diisi',
+            'email.email'  => 'Format email tidak sesuai',
+            'alamat.required'  => 'Alamat wajib diisi',
+        ];
+        $validated = $this->validate($rules, $pesan);
+        $data = ModelsEmployee::find($this->employee_id);
+        $data->update($validated);
+        session()->flash('message','data berhasil diupdate');
+
+        $this->clear();
+    }
+
+    public function clear()
+    {
+        $this->nama = '';
+        $this->email = '';
+        $this->alamat = '';
+        $this->updateData = false;
+        $this->employee_id = '';
     }
 
     public function render()
--- resources/views/livewire/employee.blade.php
+++ resources/views/livewire/employee.blade.php
@@ -43,7 +43,14 @@
             </div>
             <div class="mb-3 row">
                 <label class="col-sm-2 col-form-label"></label>
-                <div class="col-sm-10"><button type="button" class="btn btn-primary" name="submit" wire:click = "store">SIMPAN</button>
+                <div class="col-sm-10">
+                    @if ($updateData == false)
+                        <button type="button" class="btn btn-primary" name="submit" wire:click = "store()">SIMPAN</button>
+                    @else
+                        <button type="button" class="btn btn-primary" name="submit" wire:click = "update()">UPDATE</button>
+                    @endif
+                        <button type="button" class="btn btn-secondary" name="submit" wire:click = "clear()">CLEAR</button>
+                    
                 </div>
             </div>
         </form>
@@ -74,7 +81,7 @@
                     <td>{{ $value->email }}</td>
                     <td>{{ $value->alamat }}</td>
                     <td>
-                        <a href="" class="btn btn-warning btn-sm">Edit</a>
+                        <a wire:click="edit({{ $value->id }})" class="btn btn-warning btn-sm">Edit</a>
                         <a href="" class="btn btn-danger btn-sm">Del</a>
                     </td>
                 </tr>
